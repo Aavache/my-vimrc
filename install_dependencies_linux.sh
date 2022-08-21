1d996e1fb0f50ad3010a6ea1dd01f6f1b085cb8a
Aaron Valero <aaron.valero@outlook.com>
2022-08-21 20:51:01 +0900

    installing dependencies bash scripts

---
+++ install_dependencies_linux.sh
@@ -0,0 +1,6 @@
+# Installing the vim-plug manager
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Installing silver_search for unlocking fzf features
+apt-get install silversearcher-ag
+
---
+++ install_dependencies_mac.sh
@@ -0,0 +1,5 @@
+# Installing vim-plug for managing plugins:
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Installing the_silver_search for unlocking fzf features
+brew install the_silver_searcher
